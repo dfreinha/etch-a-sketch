f75016e44d0097227b95892ab4de98655027c289
Dan Reinhardt <58948625+dfreinha@users.noreply.github.com>
2025-06-10 08:55:42 -0700

    add event to change color on mouse exit adjust comments

--- script.js
+++ script.js
@@ -6,14 +6,18 @@ function createGrid(gridSize) {
     grid.innerHTML = ''; // Remove existing cells
     const cellSize = containerSize / gridSize;
 
-    for (let i = 0; i < gridSize * gridSize; i++) {
+    for (let i = 0; i < gridSize * gridSize; i++) { // loop builds grid using variable cell size based on input given
       const cell = document.createElement('div');
       cell.classList.add('cell');
       cell.style.width = `${cellSize}px`;
       cell.style.height = `${cellSize}px`;
 
-      cell.addEventListener('mouseenter', () => {
+      cell.addEventListener('mouseenter', () => { // event to change color when mouse enters cells
         cell.style.backgroundColor = '#cd57ff';
+      });
+
+      cell.addEventListener('mouseout', () => { // event to change color when mouse exit cells
+        cell.style.backgroundColor = '#4433ff';
       });
 
       grid.appendChild(cell);
@@ -21,12 +25,12 @@ function createGrid(gridSize) {
   }
   changeButton.addEventListener('click', () => {
     let input = prompt('Enter number of squares per side (max 100):');
-    let gridSize = parseInt(input);
+    let gridSize = parseInt(input); // insure integer for grid size decimal truncated
 
-    if (!isNaN(gridSize) && gridSize > 0 && gridSize <= 100) {
+    if (!isNaN(gridSize) && gridSize > 0 && gridSize <= 100) { // checks in value is a positive  and <= 100 
       createGrid(gridSize);
     } else {
-      alert('Please enter a valid number between 1 and 100.');
+      alert('Please enter a valid number between 1 and 100.'); // redo note
     }
   });
 
